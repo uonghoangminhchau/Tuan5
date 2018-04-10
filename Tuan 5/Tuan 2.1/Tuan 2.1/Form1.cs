317ec37efeeccb60a84c54259311e365411ae947
Chau <uonghoangminhchau@gmail.com>
2018-04-10 20:27:27 +0700

    xong

---
+++ Tuan 5/Tuan 2.1/Tuan 2.1/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace Tuan_2._1
+{
+    public partial class Form1 : Form
+    {
+        IPEndPoint ipep;
+        Socket newsock;
+        byte[] data = new byte[1024];
+        int recv;
+        Socket client;
+        public Form1()
+        {
+            InitializeComponent();
+            ipep = new IPEndPoint(IPAddress.Any, 9050);
+            newsock = new Socket(AddressFamily.InterNetwork,
+                            SocketType.Stream, ProtocolType.Tcp);
+            newsock.Bind(ipep);
+            newsock.Listen(10);
+            client = newsock.Accept();
+            listBox1.Items.Add("Waiting for a client...");
+            //client.Connect(ipep);
+            IPEndPoint clientep = (IPEndPoint)client.RemoteEndPoint;
+            textBox1.Text = clientep.Address.ToString();
+            listBox1.Items.Add("Connected with " + clientep.Address +" at port"+
+            clientep.Port);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+                data = new byte[1024];
+                data = Encoding.ASCII.GetBytes(textBox2.Text);
+                client.Send(data,data.Length,SocketFlags.None);
+                listBox1.Items.Add(textBox2.Text.ToString());
+                TimeSpan time = TimeSpan.FromSeconds(1);
+                IAsyncResult result = client.BeginReceive(data, 0, data.Length, SocketFlags.None, null, null);
+                result.AsyncWaitHandle.WaitOne(time);
+                if (result.IsCompleted)
+                {
+                    recv = client.EndReceive(result);
+                    string stringData = Encoding.ASCII.GetString(data, 0, recv);
+                    listBox1.Items.Add(stringData.Substring(0, recv));
+                }
+        }
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+        }
+
+    }
+}
---
+++ Tuan-2-master/Tuan 2.1/Client/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class Program
+    {
+        static byte[] data = new byte[1080];
+        static IAsyncResult result(TimeSpan a, Socket s, byte[] data)
+        {
+            IAsyncResult result = s.BeginReceive(data, 0, data.Length, SocketFlags.None, new AsyncCallback(ReceivedData), s);
+            return result;
+        }
+        static void ReceivedData(IAsyncResult iar)
+        {
+            Socket remote = (Socket)iar.AsyncState;
+            int recv = remote.EndReceive(iar);
+            string receivedData = Encoding.ASCII.GetString(data, 0, recv);
+        }
+        static void Main(string[] args)
+        {
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9050);
+            Socket newsock = new Socket(AddressFamily.InterNetwork,
+                            SocketType.Stream, ProtocolType.Tcp);
+            TimeSpan time = TimeSpan.FromSeconds(1);
+            
+            try
+            {
+                newsock.Connect(ipep);
+                Console.WriteLine("connect thanh cong");
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine("connect that bai");
+            }
+            
+            while (true)
+            {
+                IAsyncResult result = newsock.BeginReceive(data, 0, data.Length, SocketFlags.None, null, null);
+                result.AsyncWaitHandle.WaitOne(time);
+                data = new byte[1024];
+                string str = Console.ReadLine();
+                data = Encoding.ASCII.GetBytes(str);
+                newsock.Send(data,data.Length,SocketFlags.None);
+                if (result.IsCompleted)
+                {
+                    int rev = newsock.EndReceive(result);
+                    string str1 = Encoding.ASCII.GetString(data);
+                    Console.WriteLine(str1);
+                }
+            }
+        }
+    }
+}
